b7102949783fb72a7e88f2399fe52f25325eec4c
juniorjasin <jasinjunior@gmail.com>
2018-11-12 21:15:38 -0300

    fix put reclamo & add cors

--- ReclamosService/src/ar/edu/ubp/das/resources/ReclamosResource.java
+++ ReclamosService/src/ar/edu/ubp/das/resources/ReclamosResource.java
@@ -19,6 +19,7 @@ import javax.xml.ws.WebServiceException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 
 import ar.edu.ubp.das.beans.ReclamoBean;
 import ar.edu.ubp.das.config.Config;
@@ -29,6 +30,8 @@ import ar.edu.ubp.das.ws.UsuarioBean;
 @Path("/reclamos")
 @Produces(MediaType.APPLICATION_JSON)
 public class ReclamosResource {
+
+	private final int CONSTRAINT_ERROR = 547;
 
 	@GET
 	public Response getReclamos() {
@@ -75,21 +78,19 @@ public class ReclamosResource {
 
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateReclamo(Object body) throws SQLException {
-
-		JsonObject jobj = new Gson().fromJson(body.toString(), JsonObject.class);
+	public Response updateReclamo(String body) throws SQLException {
 
 		Integer nro_reclamo;
 		String respuesta;
 		Integer resp_respuesta;
 
 		try {
+			JsonObject jobj = new Gson().fromJson(body.toString(), JsonObject.class);
 			nro_reclamo = Integer.valueOf(jobj.get("nro_reclamo").toString());
 			respuesta = jobj.get("respuesta").toString();
 			resp_respuesta = Integer.valueOf(jobj.get("resp_respuesta").toString());
-			// System.out.println(nro_reclamo + ", " + respuesta + ", " + resp_respuesta);
 
-		} catch (NullPointerException ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return Response.status(Response.Status.BAD_REQUEST).entity("Body incorrecto").build();
 		}
@@ -109,41 +110,57 @@ public class ReclamosResource {
 			st.setInt(1, nro_reclamo);
 			st.setString(2, respuesta);
 			st.setInt(3, resp_respuesta);
-			st.execute();
+
+			int result = st.executeUpdate();
+
+			if (result < 1) {
+				return Response.status(Response.Status.BAD_REQUEST).entity("nro reclamo mal").build();
+			}
+
 			conn.commit();
 
 			return Response.status(Response.Status.OK).build();
 
-		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException ex) {
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException ex) {
 			conn.rollback();
 			ex.printStackTrace();
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error interno del servidor").build();
+
+		} catch (SQLServerException ex) {
+			ex.printStackTrace();
+			if (ex.getErrorCode() == CONSTRAINT_ERROR) {
+				return Response.status(Response.Status.BAD_REQUEST).entity("Constraint").build();
+			}
+
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error interno del servidor").build();
+
 		} finally {
 			conn.close();
 		}
 	}
-	
+
 	@Path("/login")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getUsuario(Object body) {		
+	public Response getUsuario(Object body) {
 		JsonObject jobj = new Gson().fromJson(body.toString(), JsonObject.class);
-		
-		String usuario, clave;		
+
+		String usuario, clave;
 		try {
 			usuario = jobj.get("usuario").toString();
 			clave = jobj.get("clave").toString();
-		}catch (NullPointerException ex){
+		} catch (NullPointerException ex) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("Body incorrecto").build();
 		}
-		
+
 		LoginWSService service = new LoginWSService();
 		LoginWS login = service.getLoginWSPort();
 		try {
 			UsuarioBean usr = login.getUsuario(usuario, clave);
 			return Response.status(Response.Status.OK).entity(usr).build();
-		}catch(WebServiceException e) {			
-			return Response.status(Response.Status.UNAUTHORIZED).entity("{\"error\":\"Password o usuario incorrectos\"}").type("application/json").build();
+		} catch (WebServiceException e) {
+			return Response.status(Response.Status.UNAUTHORIZED)
+					.entity("{\"error\":\"Password o usuario incorrectos\"}").type("application/json").build();
 		}
 	}
 }
